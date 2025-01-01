f0ce7742696851d22be5199cd62336f063f66b78
Florin Miron <mironflorin123@yahoo.com>
2025-01-01 16:05:52 +0000

    Adjust the persons selection dropdown container to fit the dynamically added content

--- dist/index.850bd9e5.js
+++ dist/index.850bd9e5.js
@@ -605,18 +605,32 @@ var _searchFormViewJs = require("./views/searchFormView.js");
 var _searchFormViewJsDefault = parcelHelpers.interopDefault(_searchFormViewJs);
 var _headerContentViewJs = require("./views/headerContentView.js");
 var _headerContentViewJsDefault = parcelHelpers.interopDefault(_headerContentViewJs);
+var _personsSelectionViewJs = require("./views/flights-search-form/personsSelectionView.js");
+var _personsSelectionViewJsDefault = parcelHelpers.interopDefault(_personsSelectionViewJs);
+const controlSelectPersons = function() {
+    // Generate persons selection markup
+    (0, _personsSelectionViewJsDefault.default)._generateAdultsMarkup();
+    (0, _personsSelectionViewJsDefault.default)._generateChildrenMarkup();
+    // Set dynamic styling for children age selectors parent element
+    (0, _personsSelectionViewJsDefault.default)._hideChildrenSelectAgeParentEl();
+    // Add the functionality for selecting the number of persons
+    (0, _personsSelectionViewJsDefault.default)._adultsCounter();
+    (0, _personsSelectionViewJsDefault.default)._childrenCounter();
+    // Add the functionality for selection done
+    (0, _searchFormViewJsDefault.default)._hideSelectionDropdown();
+};
 const init = function() {
     (0, _navbarViewJsDefault.default).setDynamicStyling();
+    (0, _personsSelectionViewJsDefault.default)._addHandlerRender(controlSelectPersons);
     (0, _sideNavbarViewJsDefault.default).setDynamicStyling();
     (0, _sideNavbarViewJsDefault.default).toggleSideNavbar();
-    (0, _searchFormViewJsDefault.default).setDropdownDynamicStyling();
-    (0, _searchFormViewJsDefault.default).showSelectionDropdown();
-    (0, _searchFormViewJsDefault.default).hideSelectionDropdown();
+    (0, _searchFormViewJsDefault.default)._setDropdownDynamicStyling();
+    (0, _searchFormViewJsDefault.default)._showSelectionDropdown();
     (0, _headerContentViewJsDefault.default).setDynamicStyling();
 };
 init();
 
-},{"./views/sideNavbarView.js":"9BkUd","./views/navbarView.js":"9sJsi","./views/searchFormView.js":"gHYzF","./views/headerContentView.js":"d8zti","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"9BkUd":[function(require,module,exports,__globalThis) {
+},{"./views/sideNavbarView.js":"9BkUd","./views/navbarView.js":"9sJsi","./views/searchFormView.js":"gHYzF","./views/headerContentView.js":"d8zti","./views/flights-search-form/personsSelectionView.js":"dn95k","@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"9BkUd":[function(require,module,exports,__globalThis) {
 var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
 parcelHelpers.defineInteropFlag(exports);
 class SideNavbarView {
@@ -715,7 +729,7 @@ class SearchFormView {
     personsSelectionDropdown = document.querySelector(".persons-selection");
     flightClassDropdown = document.querySelector(".flight-class-selection");
     selectionDoneBtns = document.querySelectorAll(".selection-done-btn");
-    setDropdownDynamicStyling() {
+    _setDropdownDynamicStyling() {
         [
             "load",
             "resize"
@@ -724,13 +738,13 @@ class SearchFormView {
                 const parentSizes = this.dropdownInputsContainer.getBoundingClientRect();
                 const selectPersonsBtnSizes = this.flightPersonsDropdownBtn.getBoundingClientRect();
                 const selectFlightClassBtnSizes = this.flightClassDropdownBtn.getBoundingClientRect();
-                this.personsSelectionDropdown.style.top = `${selectPersonsBtnSizes.height}px`;
-                this.flightClassDropdown.style.top = `${parentSizes.height}px`;
+                this.personsSelectionDropdown.style.top = `${selectPersonsBtnSizes.height + 10}px`;
+                this.flightClassDropdown.style.top = `${parentSizes.height + 10}px`;
                 this.flightClassDropdown.style.left = `${selectFlightClassBtnSizes.left - parentSizes.left}px`;
             });
         });
     }
-    showSelectionDropdown() {
+    _showSelectionDropdown() {
         this.dropdownInputsContainer.addEventListener("click", (e)=>{
             const visibleDropdowns = document.querySelectorAll(".dropdown-visible");
             if (e.target.closest("#flight-persons-dropdown-btn")) {
@@ -745,7 +759,7 @@ class SearchFormView {
             }
         });
     }
-    hideSelectionDropdown() {
+    _hideSelectionDropdown() {
         this.selectionDoneBtns.forEach((btn)=>{
             btn.addEventListener("click", (e)=>{
                 e.target.closest(".dropdown-visible").classList.remove("dropdown-visible");
@@ -775,6 +789,178 @@ class HeaderContentView {
     }
 }
 exports.default = new HeaderContentView();
+
+},{"@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"dn95k":[function(require,module,exports,__globalThis) {
+var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
+parcelHelpers.defineInteropFlag(exports);
+class PersonsSelectionView {
+    _parentEl = document.querySelector(".persons-selection");
+    _adultsParentEl = document.querySelector(".adults");
+    _childrenParentEl = document.querySelector(".children");
+    _childrenAgeParentEl = document.querySelector(".selection-children-age");
+    _adults = 1;
+    _children = 0;
+    _childrenSelectAgeElements = [];
+    _addHandlerRender(handler) {
+        window.addEventListener("load", handler);
+    }
+    _clearMarkup(container) {
+        container.innerHTML = "";
+    }
+    _generateAdultsMarkup() {
+        this._clearMarkup(this._adultsParentEl);
+        const markup = `
+            <div class="selection-content d-flex flex-column">
+                <h3 class="fs-5 fw-semibold">Adults</h3>
+                <p class="m-0">Age 18+</p>
+            </div>
+            <div class="selection-number d-flex align-items-center">
+                <button
+                type="button"
+                class="selection-decrease-btn border-0 rounded-3 focus-ring"
+                ${this._adults === 1 ? "disabled" : ""}
+                >
+                <i class="fa-solid fa-minus"></i>
+                </button>
+                <span class="selection-count-adults">${this._adults}</span>
+                <button
+                type="button"
+                class="selection-increase-btn border-0 rounded-3 focus-ring"
+                ${this._adults + this._children === 9 ? "disabled" : ""}
+                >
+                <i class="fa-solid fa-plus"></i>
+                </button>
+            </div>
+    `;
+        this._adultsParentEl.insertAdjacentHTML("afterbegin", markup);
+    }
+    _generateChildrenMarkup() {
+        this._clearMarkup(this._childrenParentEl);
+        const markup = `
+            <div class="selection-content">
+                <h3 class="fs-5 fw-semibold">Children</h3>
+                <p class="m-0">Age 0 - 17</p>
+            </div>
+            <div class="selection-number d-flex align-items-center">
+                <button
+                type="button"
+                class="selection-decrease-btn border-0 rounded-3 focus-ring"
+                ${this._children === 0 ? "disabled" : ""}
+                >
+                <i class="fa-solid fa-minus"></i>
+                </button>
+                <span class="selection-count-children">${this._children}</span>
+                <button
+                type="button"
+                class="selection-increase-btn border-0 rounded-3 focus-ring"
+                ${this._adults + this._children === 9 ? "disabled" : ""}
+                >
+                <i class="fa-solid fa-plus"></i>
+                </button>
+            </div>
+    `;
+        this._childrenParentEl.insertAdjacentHTML("afterbegin", markup);
+    }
+    _generateSelectChildAgeMarkup(counter) {
+        const selectMarkup = `
+                 <label class="fs-5 fw-semibold" for="child-age-${counter}"
+                        >${counter}${this._childrenCount(counter)} child's age</label
+              >
+              <select
+                name="${this._childrenCount(counter)}-child-age"
+                id="child-age-${counter}"
+                class="px-3 py-2 rounded-3 focus-ring mb-3"
+              >
+                <option value="">Select age at time of flying</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+                <option value="11">11</option>
+                <option value="12">12</option>
+                <option value="13">13</option>
+                <option value="14">14</option>
+                <option value="15">15</option>
+                <option value="16">16</option>
+                <option value="17">17</option>
+              </select>
+          `;
+        this._childrenSelectAgeElements.push(selectMarkup);
+    }
+    _displaySelectChildAgeEl() {
+        this._clearMarkup(this._childrenAgeParentEl);
+        const markup = this._childrenSelectAgeElements.map((el)=>{
+            return el;
+        }).join("");
+        this._childrenAgeParentEl.insertAdjacentHTML("afterbegin", markup);
+    }
+    _adultsCounter() {
+        this._adultsParentEl.addEventListener("click", (e)=>{
+            if (e.target.closest(".selection-increase-btn")) {
+                this._adults++;
+                this._generateAdultsMarkup();
+                this._generateChildrenMarkup();
+            }
+            if (e.target.closest(".selection-decrease-btn")) {
+                this._adults--;
+                this._generateAdultsMarkup();
+                this._generateChildrenMarkup();
+            }
+        });
+    }
+    _childrenCounter() {
+        this._childrenParentEl.addEventListener("click", (e)=>{
+            if (e.target.closest(".selection-increase-btn")) {
+                this._children++;
+                this._generateSelectChildAgeMarkup(this._children);
+                this._showChildrenSelectAgeParentEl();
+                this._displaySelectChildAgeEl();
+                this._generateChildrenMarkup();
+                this._generateAdultsMarkup();
+            }
+            if (e.target.closest(".selection-decrease-btn")) {
+                this._children--;
+                this._removeChildAgeEl(this._childrenSelectAgeElements);
+                this._displaySelectChildAgeEl();
+                this._generateChildrenMarkup();
+                this._generateAdultsMarkup();
+            }
+        });
+    }
+    // ==================
+    // Utility Methods
+    // ==================
+    _hideChildrenSelectAgeParentEl() {
+        this._childrenAgeParentEl.style.display = "none";
+    }
+    _showChildrenSelectAgeParentEl() {
+        this._childrenAgeParentEl.style.display = "flex";
+    }
+    _removeChildAgeEl(array) {
+        array.pop();
+        if (array.length === 0) this._hideChildrenSelectAgeParentEl();
+        return array;
+    }
+    _childrenCount(count) {
+        switch(true){
+            case count === 1:
+                return "st";
+            case count === 2:
+                return "nd";
+            case count === 3:
+                return "rd";
+            default:
+                return "th";
+        }
+    }
+}
+exports.default = new PersonsSelectionView();
 
 },{"@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}]},["4nnrR","1GgH0"], "1GgH0", "parcelRequire94c2")
 
