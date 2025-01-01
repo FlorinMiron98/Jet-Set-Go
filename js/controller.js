50dbea16b50d3ddd438257ef5b478e7dba78f9d7
Florin Miron <mironflorin123@yahoo.com>
2025-01-01 16:04:11 +0000

    Create the control function for selecting persons

--- js/controller.js
+++ js/controller.js
@@ -2,14 +2,31 @@ import sideNavbarView from "./views/sideNavbarView.js";
 import navbarView from "./views/navbarView.js";
 import searchFormView from "./views/searchFormView.js";
 import headerContentView from "./views/headerContentView.js";
+import personsSelectionView from "./views/flights-search-form/personsSelectionView.js";
+
+const controlSelectPersons = function () {
+  // Generate persons selection markup
+  personsSelectionView._generateAdultsMarkup();
+  personsSelectionView._generateChildrenMarkup();
+
+  // Set dynamic styling for children age selectors parent element
+  personsSelectionView._hideChildrenSelectAgeParentEl();
+
+  // Add the functionality for selecting the number of persons
+  personsSelectionView._adultsCounter();
+  personsSelectionView._childrenCounter();
+
+  // Add the functionality for selection done
+  searchFormView._hideSelectionDropdown();
+};
 
 const init = function () {
   navbarView.setDynamicStyling();
+  personsSelectionView._addHandlerRender(controlSelectPersons);
   sideNavbarView.setDynamicStyling();
   sideNavbarView.toggleSideNavbar();
-  searchFormView.setDropdownDynamicStyling();
-  searchFormView.showSelectionDropdown();
-  searchFormView.hideSelectionDropdown();
+  searchFormView._setDropdownDynamicStyling();
+  searchFormView._showSelectionDropdown();
   headerContentView.setDynamicStyling();
 };
 
