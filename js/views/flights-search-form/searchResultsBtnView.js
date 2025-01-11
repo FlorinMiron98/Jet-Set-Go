d8c877291d40d566dab1956b6dd3223d578af87e
Florin Miron <mironflorin123@yahoo.com>
2025-01-11 17:24:05 +0000

    Create the queryStringValues object and test its functionality in the checkValues method

--- js/views/flights-search-form/searchResultsBtnView.js
+++ js/views/flights-search-form/searchResultsBtnView.js
@@ -5,30 +5,62 @@ import arrivalLocationSearchView from "./arrivalLocationSearchView";
 import datePickerView from "./datePickerView";
 
 class SearchResultsBtnView {
-  _searchBtn = document.querySelector(".search-button");
+  _parentEl = document.querySelector(".inputs-container");
+
+  _queryStringValues = {
+    persons: {
+      adults: 0,
+      children: [],
+    },
+    flightClass: "",
+    departureLocationId: "",
+    arrivalLocationId: "",
+    departureDate: "",
+    // Dynamically check if the return date was selected by the user and assign the value
+    ...(datePickerView._queryValues.returnDate && {
+      returnDate: datePickerView._queryValues.returnDate,
+    }),
+  };
 
   _addHandlerCreateQueries(handler) {
-    this._searchBtn.addEventListener("click", () => {
-      handler();
+    this._parentEl.addEventListener("click", (e) => {
+      if (e.target.classList.contains("search-button")) {
+        handler();
+      }
+      return;
     });
   }
 
   _checkValues() {
+    this._queryStringValues.persons.adults = personsSelectionView._adults;
+
     for (const el of personsSelectionView._childrenSelectAgeEl) {
       if (!el.value) {
         console.log("Please select children age");
+      } else {
+        this._queryStringValues.persons.children.push(+el.value);
       }
       console.log(el.value);
     }
 
-    console.log(flightClassSelectionView._flightClassQuery);
+    this._queryStringValues.flightClass =
+      flightClassSelectionView._flightClassQuery;
 
-    console.log(departureLocationSearchView._departureLocationId);
+    this._queryStringValues.departureLocationId =
+      departureLocationSearchView._departureLocationId;
 
-    console.log(arrivalLocationSearchView._arrivalLocationId);
+    this._queryStringValues.arrivalLocationId =
+      arrivalLocationSearchView._arrivalLocationId;
 
-    console.log(datePickerView._queryValues.departureDate.formattedDate);
-    console.log(datePickerView._queryValues.returnDate.formattedDate);
+    this._queryStringValues.departureDate =
+      datePickerView._queryValues.departureDate.formattedDate
+        .split("/")
+        .join("-");
+
+    this._queryStringValues.returnDate =
+      datePickerView._queryValues.returnDate.formattedDate.split("/").join("-");
+
+    console.log(this._queryStringValues);
   }
 }
 
