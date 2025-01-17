9c3ce39a33288f2163ac8eafa42b2faaabff3bac
Florin Miron <mironflorin123@yahoo.com>
2025-01-17 21:09:12 +0000

    Add the side navbar and the functionlity to display it to the flights-results.html

--- js/flightResultsController.js
+++ js/flightResultsController.js
@@ -1,13 +1,22 @@
+// Import model
 import * as model from "./model";
 
+// Import views
 import flightResultsView from "./views/flightResultsView";
+import navbarView from "./views/navbarView";
+import sideNavbarView from "./views/sideNavbarView";
 
-const controlLoadURL = function () {
-  flightResultsView._getQueryParameters();
+const controlOnLoadSearch = function () {
+  model.loadFlightsSearchResults(flightResultsView._getQueryParameters());
 };
 
 const init = function () {
-  flightResultsView._addHandlerLoad(controlLoadURL);
+  flightResultsView._addHandlerRender(controlOnLoadSearch);
+
+  // Dynamic styling
+  navbarView.setDynamicStyling();
+  sideNavbarView.setDynamicStyling();
+  sideNavbarView.toggleSideNavbar();
 };
 
 init();
