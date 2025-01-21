4e2eece9b80beaf452026d3d0a563d649b95fb13
Florin Miron <mironflorin123@yahoo.com>
2025-01-21 16:29:00 +0000

    Create the renderSpinner and clearMarkup methods

--- js/views/flights-search-results/flightsOffersView.js
+++ js/views/flights-search-results/flightsOffersView.js
@@ -4,4 +4,19 @@ class FlightsOffersView {
 
   // Global variables
   _errorMessage = "No flight offers matching your request. Please try again!";
+
+  _renderSpinner() {
+    const markup = `
+        <div class="loader-container d-flex flex-column align-items-center justify-content-center mt-4">
+              <div class="loader"></div>
+              <p class="mt-2">Searching for flights offers...</p>
+        </div>
+    `;
+    this._clearMarkup();
+    this._parentEl.insertAdjacentElement("afterbegin", markup);
+  }
+
+  _clearMarkup() {
+    this._parentEl.innerHTML = "";
+  }
 }
