bbbe5169bb65e89277cff8a4f29da9f73f33ddcf
Florin Miron <mironflorin123@yahoo.com>
2025-01-21 16:28:31 +0000

    Create the flightsOffersView

---
+++ js/views/flights-search-results/flightsOffersView.js
@@ -0,0 +1,7 @@
+class FlightsOffersView {
+  // DOM Elements
+  _parentEl = document.querySelector(".flights-search-results");
+
+  // Global variables
+  _errorMessage = "No flight offers matching your request. Please try again!";
+}
