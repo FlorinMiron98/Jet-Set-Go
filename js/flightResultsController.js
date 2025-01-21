2b7329e6d9f2fab75e52d971290bc5e3549e8459
Florin Miron <mironflorin123@yahoo.com>
2025-01-21 16:05:22 +0000

    Create the controllers for displaying and hiding the filters dialog

--- js/flightResultsController.js
+++ js/flightResultsController.js
@@ -6,13 +6,24 @@ import flightResultsView from "./views/flightResultsView";
 import navbarView from "./views/navbarView";
 import sideNavbarView from "./views/sideNavbarView";
 import headerContentView from "./views/headerContentView";
+import filtersBtnView from "./views/flights-search-results/filtersBtnView";
 
 const controlOnLoadSearch = function () {
   model.loadFlightsSearchResults(flightResultsView._getQueryParameters());
 };
 
+const controlDisplayDialog = function () {
+  filtersBtnView._displayDialog();
+};
+
+const controlHideDialog = function () {
+  filtersBtnView._hideDialog();
+};
+
 const init = function () {
   flightResultsView._addHandlerRender(controlOnLoadSearch);
+  filtersBtnView._addHandlerDisplayDialog(controlDisplayDialog);
+  filtersBtnView._addHandlerHideDialog(controlHideDialog);
 
   // Dynamic styling
   navbarView.setDynamicStyling();
