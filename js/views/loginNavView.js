d5659ca14e6e7cdc40e7e73ae68e31c097a667fe
Florin Miron <mironflorin123@yahoo.com>
2025-05-06 20:08:51 +0100

    Create the loginNavView

---
+++ js/views/loginNavView.js
@@ -0,0 +1,28 @@
+class loginNavView {
+  _registerBtn = document.querySelector(".register-btn");
+  _loginBtn = document.querySelector(".login-btn");
+  _registerDialog = document.querySelector(".register-dialog");
+  _loginDialog = document.querySelector(".login-dialog");
+
+  _addHandlerDisplayRegisterDialog(handler) {
+    this._registerBtn.addEventListener("click", () => {
+      handler();
+    });
+  }
+
+  _addHandlerDisplayLoginDialog(handler) {
+    this._loginBtn.addEventListener("click", () => {
+      handler();
+    });
+  }
+
+  _displayRegisterDialog() {
+    this._registerDialog.showModal();
+    document.body.classList.add("no-scroll");
+  }
+
+  _displayLoginDialog() {
+    this._loginDialog.showModal();
+    document.body.classList.add("no-scroll");
+  }
+}
